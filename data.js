f60881266c1ed460bd0d5fc9eaa145acdc1c24ba
MelbSummer <144795676+MelbSummer@users.noreply.github.com>
2023-09-13 16:51:30 +0900

    230913 htmlcss

---
+++ data.js
@@ -0,0 +1,85 @@
+const langData = []
+
+
+const iconData = [
+    {
+        icon:"fa-regular fa-star",
+        text:"최고의 전망",
+    },
+    {
+        icon:"fa-solid fa-bed",
+        text:"방",
+    },
+    {
+        icon:"fa-solid fa-hotel",
+        text:"호텔",
+    },
+    {
+        icon:"fa-regular fa-building",
+        text:"도시",
+    },
+    {
+        icon:"fa-solid fa-mug-saucer",
+        text:"B&B",
+    },
+    {
+        icon:"fa-solid fa-utensils",
+        text:"레스토랑",
+    },
+    {
+        icon:"fa-solid fa-tree",
+        text:"한적한 시골",
+    },
+    {
+        icon:"fa-solid fa-wine-bottle",
+        text:"와인 농장",
+    },
+    {
+        icon:"fa-solid fa-otter",
+        text:"동물원",
+    },
+    {
+        icon:"fa-solid fa-gamepad",
+        text:"키즈",
+    },
+    {
+        icon:"fa-solid fa-tower-observation",
+        text:"타워",
+    },
+    {
+        icon:"fa-solid fa-mountain",
+        text:"고지대",
+    },
+    {
+        icon:"fa-solid fa-palette",
+        text:"전시",
+    },
+    {
+        icon:"fa-solid fa-golf-ball-tee",
+        text:"골프장",
+    },
+    {
+        icon:"fa-solid fa-key",
+        text:"신규",
+    },
+    {
+        icon:"fa-solid fa-house-tsunami",
+        text:"호숫가",
+    },
+    {
+        icon:"fa-solid fa-person-swimming",
+        text:"해변 근처",
+    },
+    {
+        icon:"fa-solid fa-campground",
+        text:"캠프",
+    },
+    {
+        icon:"fa-solid fa-sailboat",
+        text:"보트",
+    },
+    {
+        icon:"fa-solid fa-ship",
+        text:"섬",
+    },
+]
